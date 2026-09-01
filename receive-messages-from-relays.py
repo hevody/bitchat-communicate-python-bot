cb8ca7f91eb2fc951981b88c521581fbf8172789
Velocity <145270377+hevody@users.noreply.github.com>
2026-09-01 21:43:04 +0000

    add: receive-messages-from-relays.py

--- receive-messages-from-relays.py
+++ receive-messages-from-relays.py
@@ -1,3 +1,34 @@
 import json
-import websocket
+import asyncio
+from websockets.asyncio.client import connect
+from websockets.exceptions import ConnectionClosed
 
+with open('settings.json') as f:
+	settings = json.load(fp=f)
+
+RELAYS = settings["RELAYS"]
+
+SUBSC_RELAY_MESSAGE = json.dumps([
+	"REQ", "global-bitchat",
+	{"kinds": [20000]}
+])
+
+
+async def client(relay: str):
+	async for websocket in connect(relay):
+		try:
+			await websocket.send(SUBSC_RELAY_MESSAGE)
+			msg = await websocket.recv()
+
+			message_jsonify = json.loads(msg)
+			print(message_jsonify)
+		except ConnectionClosed:
+			continue
+
+async def main():
+	await asyncio.gather(
+		*(client(relay) for relay in RELAYS)
+	)
+
+if __name__ == '__main__':
+	asyncio.run(main())
