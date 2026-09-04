935f8d9024ab4fe41c1c6bde03655aeb744cfea5
Velocity <145270377+hevody@users.noreply.github.com>
2026-09-04 15:29:00 +0000

    feat: working nonce miner

--- mining_nonce_gen.py
+++ mining_nonce_gen.py
@@ -1,6 +1,96 @@
 import hashlib
 import json
 
+# return: created_at, nonce_list, id  
+# interested: mined_nonce
+
+
+
+
+
+with open('settings.json') as f:
+	settings = json.load(fp=f)
+
+nonce_difficulty = int(settings["nonce_difficulty"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main(pubkey: str, kind: int, tags: list, content: str) -> tuple[str]:
+	nonce_list = ['nonce', '', str(nonce_difficulty)]		# this is to be appended to tags[-1]
+	tags.append([])
+
+	# for testing purposes:
+	# there will be a value for created_at
+	created_at = 1788482388 
+
+	nonce_found = False
+	mined_nonce = 69489
+
+	while not nonce_found:
+		nonce_list[1] = str(mined_nonce)
+		tags[-1] = nonce_list
+
+		serialization_list_for_id = [	0,
+										pubkey,
+										created_at,
+										kind,
+										tags,
+										content
+									]
+
+		serialized = json.dumps(
+		serialization_list_for_id,
+		separators=(',', ':'),
+		ensure_ascii=False
+		)		
+
+		#print(serialized)
+
+		event_id_bytes = hashlib.sha256(serialized.encode('utf-8')).digest()
+		event_id_bits = bin(int.from_bytes(event_id_bytes, byteorder='big'))[2:].zfill(256)		
+
+		if event_id_bits[:nonce_difficulty] == '0'*nonce_difficulty:
+			nonce_found = True
+		else:
+			mined_nonce += 1	
+			nonce_found = False
+
+	print(mined_nonce)
+	event_id = hashlib.sha256(serialized.encode('utf-8')).hexdigest()
+
+
+	return created_at, tags, event_id
+
+
+		
+
+
+
+	
+	
+
+
+	
+
+	
+
+
+	
+
+
+
+
+
 
 pubkey = 'bd1a6d06654d69939336a75b2a68ac1abf060884d5cbe13bf6bd2f25dae8a214'
 created_at = 1788482388 
@@ -8,46 +98,13 @@ kind = 20000
 tags = [['g', 'wd'], ['n', 'glazer']]
 content = 'glazer po itu'
 
-
-with open('settings.json') as f:
-	settings = json.load(fp=f)
-
-nonce_difficulty = settings["nonce_difficulty"]
-
-mined_nonce = 100
-
-nonce_list = ['nonce', f'{mined_nonce}', f'{nonce_difficulty}']
-
-input(nonce_list)
-
-
-serialization_list_for_id = [0,
-						pubkey,
-						created_at,
-						kind,
-						tags,
-						content]
-
-serialized = json.dumps(
-	serialization_list_for_id,
-	separators=(',', ':'),
-	ensure_ascii=False
-	)
-
-# event_id = hashlib.sha256(serialized.encode('utf-8')).hexdigest()
-
-event_id_bytes = hashlib.sha256(serialized.encode('utf-8')).digest()
-event_id_bits = bin(int.from_bytes(event_id_bytes, byteorder='big'))[2:].zfill(256)
-
-
-
-print(event_id_bits)
-print(serialized)
-
-
-def main():
-	pass
-
 if __name__ == '__main__':
-	main()
+	get_mined_data = main(	pubkey=pubkey, 
+							kind=kind, 
+							tags=tags, 
+							content=content
+						)
+	print(get_mined_data)
+
+
 
