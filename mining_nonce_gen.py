3e358bce6cf2097e6005b8ec0e5c30f63d4141ac
Velocity <145270377+hevody@users.noreply.github.com>
2026-09-04 15:43:26 +0000

    fix: clean the mine nonce file

--- mining_nonce_gen.py
+++ mining_nonce_gen.py
@@ -1,44 +1,23 @@
 import hashlib
 import json
-
-# return: created_at, nonce_list, id  
-# interested: mined_nonce
-
-
-
-
+import time
 
 with open('settings.json') as f:
 	settings = json.load(fp=f)
 
 nonce_difficulty = int(settings["nonce_difficulty"])
 
-
-
-
-
-
-
-
-
-
-
-
-
-def main(pubkey: str, kind: int, tags: list, content: str) -> tuple[str]:
+def mine_a_nonce(pubkey: str, kind: int, tags: list, content: str) -> tuple[int, list, str]:
 	nonce_list = ['nonce', '', str(nonce_difficulty)]		# this is to be appended to tags[-1]
 	tags.append([])
 
-	# for testing purposes:
-	# there will be a value for created_at
-	created_at = 1788482388 
-
 	nonce_found = False
-	mined_nonce = 69489
+	mined_nonce = 0
 
 	while not nonce_found:
 		nonce_list[1] = str(mined_nonce)
 		tags[-1] = nonce_list
+		created_at = int(time.time())
 
 		serialization_list_for_id = [	0,
 										pubkey,
@@ -54,8 +33,6 @@ def main(pubkey: str, kind: int, tags: list, content: str) -> tuple[str]:
 		ensure_ascii=False
 		)		
 
-		#print(serialized)
-
 		event_id_bytes = hashlib.sha256(serialized.encode('utf-8')).digest()
 		event_id_bits = bin(int.from_bytes(event_id_bytes, byteorder='big'))[2:].zfill(256)		
 
@@ -65,46 +42,6 @@ def main(pubkey: str, kind: int, tags: list, content: str) -> tuple[str]:
 			mined_nonce += 1	
 			nonce_found = False
 
-	print(mined_nonce)
 	event_id = hashlib.sha256(serialized.encode('utf-8')).hexdigest()
 
-
 	return created_at, tags, event_id
-
-
-		
-
-
-
-	
-	
-
-
-	
-
-	
-
-
-	
-
-
-
-
-
-
-pubkey = 'bd1a6d06654d69939336a75b2a68ac1abf060884d5cbe13bf6bd2f25dae8a214'
-created_at = 1788482388 
-kind = 20000
-tags = [['g', 'wd'], ['n', 'glazer']]
-content = 'glazer po itu'
-
-if __name__ == '__main__':
-	get_mined_data = main(	pubkey=pubkey, 
-							kind=kind, 
-							tags=tags, 
-							content=content
-						)
-	print(get_mined_data)
-
-
-
