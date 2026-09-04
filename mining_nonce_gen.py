bf0d2ba60f4b981575dc6ece221bbb6e0a91f287
Velocity <145270377+hevody@users.noreply.github.com>
2026-09-04 02:44:02 +0000

    add: sha256 into bits

--- mining_nonce_gen.py
+++ mining_nonce_gen.py
@@ -5,8 +5,21 @@ import json
 pubkey = 'bd1a6d06654d69939336a75b2a68ac1abf060884d5cbe13bf6bd2f25dae8a214'
 created_at = 1788482388 
 kind = 20000
-tags = [['g', 'wd'], ['n', 'glazer'], ['nonce', '69489', '12']]
+tags = [['g', 'wd'], ['n', 'glazer']]
 content = 'glazer po itu'
+
+
+with open('settings.json') as f:
+	settings = json.load(fp=f)
+
+nonce_difficulty = settings["nonce_difficulty"]
+
+mined_nonce = 100
+
+nonce_list = ['nonce', f'{mined_nonce}', f'{nonce_difficulty}']
+
+input(nonce_list)
+
 
 serialization_list_for_id = [0,
 						pubkey,
@@ -15,15 +28,20 @@ serialization_list_for_id = [0,
 						tags,
 						content]
 
-
-
 serialized = json.dumps(
 	serialization_list_for_id,
-	separators=(',', ':')
+	separators=(',', ':'),
+	ensure_ascii=False
 	)
 
-event_id = hashlib.sha256(serialized.encode('utf-8')).hexdigest()
-print(event_id)
+# event_id = hashlib.sha256(serialized.encode('utf-8')).hexdigest()
+
+event_id_bytes = hashlib.sha256(serialized.encode('utf-8')).digest()
+event_id_bits = bin(int.from_bytes(event_id_bytes, byteorder='big'))[2:].zfill(256)
+
+
+
+print(event_id_bits)
 print(serialized)
 
 
