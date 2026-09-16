914e7c0542bd41316e5416a84f2c09b5063acf9e
Velocity <145270377+hevody@users.noreply.github.com>
2026-09-16 19:52:36 +0800

    fix: did not add main.py

--- main.py
+++ main.py
@@ -469,7 +469,7 @@ class Bot:
 		self.tags[2] = ["n", self.nickname]
 
 
-		self.BLOCKED_GEOHASHES = ["hrmpzfv0z5z", "6g", "SENTRYHUB", "wd", "test", "phnews", "sentryhub"]
+		self.BLOCKED_GEOHASHES = ["6g", "SENTRYHUB", "wd", "test", "phnews", "sentryhub"]
 		self.GEOHASH_CATEGORY_DATABASE = {
       "#st": "Egyptians",
       "#wd": "Filipinos",
