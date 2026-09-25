843e2177c916d3c823a8de895604caf5d5be12c5
Velocity <145270377+hevody@users.noreply.github.com>
2026-09-26 05:43:32 +0800

    fix: clarify what #wd1 is

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ def stop_bot() -> tuple[bool, str]: # need time, wd geohash, content # convert t
 	
 if __name__ == '__main__':
     if stop_bot()[0]:
-        stop_message = f"Ang bot ay pansamantalang hindi makakapagpadala ng mga mensahe hanggang {stop_bot()[1]} ... I-extend? send .stop\nPara sa karagdang impormasyon pumunta sa #wd1"
+        stop_message = f"Ang bot ay pansamantalang hindi makakapagpadala ng mga mensahe hanggang {stop_bot()[1]} ... I-extend? send .stop\n\nPara sa karagdang impormasyon pumunta sa #wd1 geohash channel"
         bot_response_stopped = Bot.main(sub_geohash=True)
         logger.info(f"Bot's response: {bot_response_stopped}")
         send_stopped_response = Sender().send(
