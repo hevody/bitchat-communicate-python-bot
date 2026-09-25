2bb5c38ce4c9ccba2c3b5a05fca5374a0cc3c8b7
Velocity <145270377+hevody@users.noreply.github.com>
2026-09-26 02:18:32 +0800

    add: proper meridian time, respond to mentions

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
-from main import Bot, Reader, Sender, Config, ProximityRelay
+from main import Bot, Reader, Sender, Config, ProximityRelay, CryptographySpecific
 import logging
 import logging.handlers
-from datetime import datetime
+from datetime import datetime, timedelta
+import os
 
 BOT_LOG_PATH = "./logs/bot.log"
 
@@ -21,6 +22,9 @@ import time
 
 TARGET_STOP_TIME = 1 * 60 * 60           # scan within seconds specified
 
+BOT_PRIVATE_KEY = os.getenv("STATIC_PRIVATE_KEY")
+BOT_PUBKEY = CryptographySpecific().retrieve_signer_and_pubkey(BOT_PRIVATE_KEY)[1]
+
 def in_target_time(iso_time: str) -> bool:
     dt = datetime.fromisoformat(iso_time)
     unix_past_time = dt.timestamp()
@@ -34,33 +38,31 @@ def in_target_time(iso_time: str) -> bool:
     else: 
         return True
 
+def get_stop_time(iso_time: str) -> str:
+    utc_time = datetime.strptime(iso_time.replace("Z", ""), "%Y-%m-%dT%H:%M:%S.%f")
+    manila_time_with_hour_increment = utc_time + timedelta(hours=8) + timedelta(hours=1)
+    with_meridian_time = manila_time_with_hour_increment.strftime("%I:%M %p").lstrip('0')
+    return with_meridian_time
 
-def stop_bot() -> bool: # need time, wd geohash, content # convert the time into unix-based time then subtract to current unix
+def stop_bot() -> tuple[bool, str]: # need time, wd geohash, content # convert the time into unix-based time then subtract to current unix
     fetched_data_from_api = Reader().main()
     if not fetched_data_from_api:
-        return False
+        return (False,)
     
     fetched_data_from_api.reverse()
 
     for fetched_datum in fetched_data_from_api:
+        if fetched_datum["nostrPubkey"] == BOT_PUBKEY:
+            continue
 
-        if fetched_datum["geohash"] == "wd" and fetched_datum["content"] == ".override" and in_target_time(fetched_datum["timestamp"]):
-            return False
-        if fetched_datum["geohash"] == "wd" and fetched_datum["content"] == ".stop" and in_target_time(fetched_datum["timestamp"]):
-            return True
-
-# in_target_time(fetched_datum["timestamp"])
-
-
-
-
-    
-
+        if fetched_datum["geohash"] == "wd" and fetched_datum["content"].endswith(".override") and in_target_time(fetched_datum["timestamp"]):
+            return (False,)
+        if fetched_datum["geohash"] == "wd" and fetched_datum["content"].endswith(".stop") and in_target_time(fetched_datum["timestamp"]):
+            return (True, get_stop_time(fetched_datum["timestamp"]))
 	
 if __name__ == '__main__':
-    stop_message = "Ang bot ay pansamantalang hindi makakapag-send ng mga mensahe sa loob ng isang oras... I-extend? .stop"
-
-    if stop_bot():
+    if stop_bot()[0]:
+        stop_message = f"Ang bot ay pansamantalang hindi makakapagpadala ng mga mensahe hanggang {stop_bot()[1]} ... I-extend? send .stop"
         send_stopped_response = Sender().send(
                                 20000,
                                 Bot().tags,
